84f653aecf632a9c17a48597e59ab27a3c79c997
Katelyn <ydatabox@gmail.com>
2024-02-16 16:37:57 -0800

    add react router dom for nav links

--- minuet/src/App.js
+++ minuet/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import NavBar from './components/navbar';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
+import ProfileFinder from './pages/profileFinder';
+import SongFinder from './pages/songFinder';
+import UserProfile from './pages/userProfile';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    // <div className="App">
+    //   <NavBar />
+    // </div>
+    <div>
+      {/* <div id='navigation'>
+        <h1 id='headerTitle'>Joe Bruin</h1>
+        <div>
+          <Link to='/'>Minuet</Link>
+          <Link to='/song-finder'>Song Finder</Link>
+          <Link to='/profile-finder'>Experience</Link>
+        </div>
+      </div> */}
+      <div id='content'>
+        <Routes>
+          <Route path='/' element={<UserProfile />} />
+          <Route path='/song-finder' element={<SongFinder />} />
+          <Route path='/profile-finder' element={<ProfileFinder />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- minuet/src/components/navbar.js
+++ minuet/src/components/navbar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import MinuetLogo from '../assets/logo.svg';
+import Button from '@mui/material/Button';
+import {Link} from 'react-router-dom';
 import './navbar.css'
+
+const isLoggedIn = false; //todo based on whether logged in or not
+
+function LogInOutButton () {
+    const buttonText = isLoggedIn ? "Log Out" : "Log In";
+    // todo: onClick send to spotify API log in
+    return <Button variant="contained" className='logInOutButton'>{buttonText}</Button>
+}
 
 function NavBar () {
     return (
-        <div>
-            <img src={MinuetLogo} alt='logo' height={80} className="navBarBrand"/>
-            <p>Minuet</p>
-            <p>Song Finder</p>
-            <p> Profile Finder</p>
+        <div className='navbar'>
+            <div className='navbarContent'>
+                <img src={MinuetLogo} alt='logo' className='navbarLogo' height={80}/> 
+                <Link to='/' className="navbarButton">Minuet</Link>
+                <Link to='/song-finder' className="navbarButton">Song Finder</Link>
+                <Link to='/profile-finder' className="navbarButton">Profile Finder</Link>
+                <LogInOutButton />
+            </div>
+           
         </div>
     );
 }
