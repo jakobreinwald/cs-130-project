c44cc4692330231f49219ea6ab4455ae412e17ae
Jed Dianlee Miguel <jedjed@Jeds-MBP.lan>
2024-02-21 12:56:28 -0800

    create landing page without MUI theme

--- minuet/src/pages/landingPage.js
+++ minuet/src/pages/landingPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import MinuetLogo from '../assets/logo.svg'
+import { Button } from '@mui/material';
+
+import './landingPage.css'
 
 function LandingPage() {
   return (
-    <div className="LandingPage">
-      <p>this is the landing page</p>
+    <div className="landingPage">
+      <div className="landingPageTwoColContent">
+        <div className="landingPageText">
+          <div>
+            <h3 className="landingPageWelcome">Welcome to</h3>
+            <div>
+              <h1 className="landingPageHeading">Minuet</h1>
+              <h2 className="landingPageSubheading">Find your sound soulmate.</h2>
+            </div>
+          </div>
+          <div className="landingPageButton">
+            <Button variant="contained" className='logInButton'>Log in with Spotify</Button>
+          </div>
+        </div>
+        <div className="langingPageLogo">
+          <img src={MinuetLogo} alt="Minuet logo" height={500} />
+        </div>
+      </div>
     </div>
   );
 }
