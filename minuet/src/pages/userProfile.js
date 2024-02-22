62f4c16fb3ab9b12b49557a97608a55fbb153bee
Maggie Li <mm302li@gmail.com>
2024-02-21 16:37:38 -0800

    Animated gradient on profile page

--- minuet/src/pages/userProfile.js
+++ minuet/src/pages/userProfile.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SongCard from '../components/songCard'
 import ProfileCard from '../components/profileCard'
 import { Box, Typography, Divider, Avatar } from '@mui/material';
 
 function UserProfile() {
   // TODO replace placeholders with actual data
-    const username = "User";
+    const username = "{username}";
     const newSongs = 5;
     const newProfiles = 3;
     const matchedSongInfo = [
@@ -26,9 +26,7 @@ function UserProfile() {
             <Box sx={{display: 'flex', flexDirection: 'row', gap: 3, maxWidth: '75%',}}>
                 <Avatar sx={{ bgcolor: 'grey.900', width: 250, height: 250, m: 5,}} />
                 <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center',}}>
-                    <Typography variant="h2" sx={{ mb: 3, background: (theme) => `linear-gradient(200deg, ${theme.palette.primary.complementary} 50%, ${theme.palette.primary.main}) 50%`, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>
-                        Hi, {username}
-                    </Typography>
+                    <AnimatedGradientText text={`Hi, ${username}`}/>
                     <Typography variant="h4" sx={{ color: 'text.primary'}}>
                         You've discovered <b>{newSongs}</b> new songs and <b>{newProfiles}</b> new profiles! Keep on exploring :)
                     </Typography>
@@ -44,3 +42,29 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
+function AnimatedGradientText( {text} ) {
+    const [gradientPosition, setGradientPosition] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setGradientPosition((prev) => (prev + 0.5) % 200);
+        }, 25); // adjust speed of movement
+    
+        return () => clearInterval(intervalId);
+    }, []);
+  
+    return (
+        <Typography variant="h2" sx={{
+            mb: 3,
+            background: (theme) => `linear-gradient(to right, ${theme.palette.primary.complementary}, ${theme.palette.primary.main}, ${theme.palette.primary.complementary})`,
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            backgroundSize: '200% 100%',
+            backgroundPosition: `${gradientPosition}% center`,
+            animation: 'none',
+        }}>
+            {text}
+        </Typography>
+    );
+};
