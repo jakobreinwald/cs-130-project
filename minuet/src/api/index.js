ba50589b7b2778f61b9c7a436bb54219ec09f61b
Jakob Reinwald <jakob.reinwald@yahoo.com>
2024-02-21 17:41:46 -0800

    git add . only stages your current folder...

---
+++ minuet/src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3001';
+
+export const fetchUsers = () => axios.get(`${url}/users`);
+
+// export const fetchUsers = () => axios.get(`${url}`);
+// export const createPost = (newPost) => axios.post(url, newPost);
+// export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+// export const deletePost = (id) => axios.delete(`${url}/${id}`);
+// export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+
+// export const getSubjects = () => axios.get(`${url}/subjects`);
+// export const getClasses = (subjectId) => axios.get(`${url}/${subjectId}/classes`);
+// export const getQuarters = (subjectId, classId) => axios.get(`${url}/${subjectId}/${classId}/quarters`);
+// export const getSpecificPosts = (subjectId, classId, quarterId) => axios.get(`${url}/${subjectId}/${classId}/${quarterId}`);
--- minuet/src/pages/profileFinder.js
+++ minuet/src/pages/profileFinder.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchUsers } from '../api';
+
 
 function ProfileFinder() {
-  return (
-    <div className="Profile">
-      <p>this is the ProfileFinder page</p>
-    </div>
-  );
+	const [data, setData] = useState('');
+	const fetchData = async () => {
+		const result = await fetchUsers();
+		setData(result.data);
+		console.log(result.data);
+	};
+
+	useEffect(() => { fetchData(); }, []);
+
+	return (
+		<div className="Profile">
+			<p>this is the ProfileFinder page</p>
+			<h1>USER LIST FOR NOW</h1>
+			{data && data.map(user => (
+				<div key={user.user_id}>
+					<p>User ID: {user.user_id}, Display Name: {user.display_name}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default ProfileFinder;
