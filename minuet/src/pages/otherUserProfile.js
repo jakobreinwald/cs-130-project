d68de0d5f245b7f8fe5c2d9fb1d3f71367309f92
Maggie Li <mm302li@gmail.com>
2024-03-15 01:08:51 -0700

    Added more frontend tests

--- minuet/src/pages/otherUserProfile.js
+++ minuet/src/pages/otherUserProfile.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Box, Typography, Divider, Avatar, Button, LinearProgress } from '@mui/material';
+import { Box, Typography, Divider, Avatar, Button } from '@mui/material';
 import { useParams} from 'react-router-dom';
 import {getUserProfile, getMatchScore} from '../api/index'
 import { Card, CardContent, Link } from '@mui/material';
+import AnimatedMatchPercentage from '../components/animatedMatchPercentage';
 
 const artist_url = 'https://open.spotify.com/artist/';
 const track_url = 'https://open.spotify.com/track/';
@@ -128,40 +129,3 @@ function ProfileTag({ tagName }) {
         </Box>
     );
 }
-
-function AnimatedMatchPercentage({ targetValue }) {
-    const [progress, setProgress] = useState(0);
-  
-    useEffect(() => {
-      const calculateIncrement = (value) => {
-        if (value < targetValue) {
-          const diff = targetValue - value;
-          return Math.min(diff / 10, targetValue); // adjust for quickness
-        }
-        return 0;
-      };
-  
-      const timer = setInterval(() => {
-        setProgress((prevProgress) => {
-          const increment = calculateIncrement(prevProgress);
-          const nextProgress = prevProgress + increment;
-          if (nextProgress >= targetValue) {
-            clearInterval(timer);
-            return targetValue;
-          }
-          return nextProgress;
-        });
-      }, 50); // adjust for faster or slower progress
-  
-      return () => {
-        clearInterval(timer);
-      };
-    }, [targetValue]);
-  
-    return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, alignItems: 'center', width: '100%' }}>
-            <LinearProgress value={progress} variant="determinate" sx={{ width: '75%', height: 15, borderRadius: 1, '& .MuiLinearProgress-bar': { backgroundImage: theme => `linear-gradient(to right, ${theme.palette.primary.complementary}, 70%, ${theme.palette.primary.main})` }}} />
-            <Typography variant="h5" sx={{ color: 'text.primary'}}> {Math.round(progress)}% Match</Typography>
-        </Box>
-    );
-  }
